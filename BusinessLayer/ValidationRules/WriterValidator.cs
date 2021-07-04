dced475e8c825ea6d78a0cb4dd1f7f1763ad723b
Rabia Nur Kavi <kavirabiaa@gmail.com>
2021-07-04 22:54:02 +0300

    MVC

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Yazar soyadını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Yazar soyadını boş geçemezsiniz.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Lütfen mailinizi girin.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Lütfen şifre girin.");
+           
+        }
+    }
+}
